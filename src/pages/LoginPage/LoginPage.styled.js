5b371b34e8ca14946c86a62ccf7f4157f26684fd
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-12-13 17:39:52 +0200

    LoginPage LoginForm

---
+++ src/pages/LoginPage/LoginPage.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  padding-top: 80px;
+  padding-bottom: auto;
+  width: 100vw;
+  height: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fdf7f2;
+
+  @media screen and (min-width: 768px) {
+  }
+
+  @media screen and (min-width: 1280px) {
+  }
+`;
