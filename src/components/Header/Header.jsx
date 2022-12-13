2e0638ba65a93251e3bebb9b31f26c197b61da72
seacat271 <seacat271083@gmail.com>
2022-12-13 16:33:35 +0200

    add icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
+import icon from '../../assets/images/icons.svg' 
 import { Logo } from "components/Logo/Logo"
 import { Navigation } from "components/Navigation/Navigation"
 export const Header = () => {
-    return ( <>
+    return ( <div style={{display: 'flex', justifyContent: 'space-between', alignItems: "center"}}>
     <Logo/>
+    <svg style={{width: '30px', height: '20px'}}>
+        <use href={icon + `#icon-menu`}></use>
+    </svg>
     <Navigation/>
-    </>)
+    </div>)
 }
