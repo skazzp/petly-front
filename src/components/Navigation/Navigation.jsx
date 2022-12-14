d05b4a3d509a42d85580f805ddcf867595de52d0
seacat271 <seacat271083@gmail.com>
2022-12-14 14:11:36 +0200

    add useSelector

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,6 @@
-import { AuthNav } from "components/AuthNav/AuthNav"
+import { AuthNav } from "components/AuthNav/AuthNav";
+import { useSelector } from "react-redux";
+import { selectToken } from "redux/auth/authSelectors";
 import { Nav } from "components/Nav/Nav"
 import { UserNav } from "components/UserNav/UserNav"
 import { NavLink } from "react-router-dom"
@@ -6,7 +8,7 @@ import * as ReactDOM from 'react-dom';
 import { Container } from "./Navigation.styled";
 
 export const Navigation = ({toggleMenu}) => {
-    const isLoggedIn = false;
+    const isLoggedIn = useSelector(selectToken);
     const modalRoot = document.querySelector("#modal-root")
     
     return(ReactDOM.createPortal(<Container>
