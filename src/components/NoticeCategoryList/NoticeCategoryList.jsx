c60d2ab13c094aad4fe79b037489d269172f4f88
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-12-14 14:40:41 +0200

    Create NoticeCategoryList.jsx

---
+++ src/components/NoticeCategoryList/NoticeCategoryList.jsx
@@ -0,0 +1,11 @@
+import NoticeCategoryItem from 'components/NoticeCategoryItem/NoticeCategoryItem';
+
+const NoticeCategoryList = () => {
+  return (
+    <ul>
+      <NoticeCategoryItem />
+    </ul>
+  );
+};
+
+export default NoticeCategoryList;
