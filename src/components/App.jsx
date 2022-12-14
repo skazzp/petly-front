aafc0379f38d84d2dcce827061ef64f30885f1a8
skazzp <skazzp@gmail.com>
2022-12-14 19:05:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
+import LoginPage from 'pages/LoginPage/LoginPage';
 import NoticesPage from 'pages/NoticesPage/NoticesPage';
 import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { LoadingMessage } from 'react-select/dist/declarations/src/components/Menu';
+import { refreshUser } from 'redux/auth/authOperation';
+import { selectIsLoading, selectToken } from 'redux/auth/authSelectors';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
 const Test = () => {
@@ -9,13 +13,15 @@ const Test = () => {
 };
 
 export const App = () => {
-  // const { isLoading } = useSelector(selectIsLoading);
-  // const isLoggedIn = useSelector(selectToken);
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   isLoggedIn && dispatch(refreshUser());
-  // }, [dispatch, isLoggedIn]);
-  return (
+  const { isLoading } = useSelector(selectIsLoading);
+  const isLoggedIn = useSelector(selectToken);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    isLoggedIn && dispatch(refreshUser());
+  }, [dispatch, isLoggedIn]);
+  return isLoading ? (
+    <h1>LOADER</h1>
+  ) : (
     <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
@@ -25,7 +31,7 @@ export const App = () => {
         <Route path="/test" element={<Test />} />
 
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/login" element={<LoadingMessage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<Test />} />
         <Route path="/friends" element={<Test />} />
         <Route path="/news" element={<Test />} />
@@ -36,7 +42,7 @@ export const App = () => {
           <Route path="favorite" element={<p>favorite</p>} />
           <Route path="own" element={<p>own</p>} />
         </Route>
-        {/* <Route path="*" element={<Navigate to={'/'} />} /> */}
+        <Route path="*" element={<Navigate to={'/'} />} />
       </Routes>
     </>
   );
