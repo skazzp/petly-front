bc4baae2b5bda570589f166c38638b5ded8fb913
seacat271 <seacat271083@gmail.com>
2022-12-15 22:40:00 +0200

    add first picture

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,10 @@
+import { Title, TitleThumb, BcgThumb, GlobalContainer } from "./HomePage.styled"
+import SmileGirl from '../../assets/images/bg/Bg-girl-mobile-1x.png'
 export const HomePage = () => {
-    return (<h1>Take good care of 
-        your small pets</h1>)
+    return (<>
+    <TitleThumb><Title>Take good care of your small pets</Title></TitleThumb>
+    <BcgThumb>
+    {/* <img href={SmileGirl} alt='smile girl'/> */}
+    </BcgThumb>
+    </>)
 }
