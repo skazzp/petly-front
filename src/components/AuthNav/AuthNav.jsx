0df57a19de67adb974c186000e9c2a1e5e28a9db
seacat271 <seacat271083@gmail.com>
2022-12-15 18:55:30 +0200

    refactoring

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,6 +1,6 @@
 import {Link, LinkInversion, AuthItem, AuthBox} from './AuthNav.styled'
 
-export const AuthNav = ({toggleMenu}) => {
+export const AuthNav = ({toggleMenu = null}) => {
 return(<AuthBox>
     <AuthItem><Link to="/login" onClick={toggleMenu}>Login</Link></AuthItem>
     <AuthItem><LinkInversion to="/register" onClick={toggleMenu}>Registrations</LinkInversion></AuthItem>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,9 @@
 import { NavTitleBox, NavTitleItem, NavTitle } from "components/Nav/Nav.styled"
 
-export const Nav = ({toggleMenu}) => {
+export const Nav = ({toggleMenu = null}) => {
     return (<NavTitleBox>
         <NavTitleItem><NavTitle to='/news' onClick={toggleMenu}>News</NavTitle></NavTitleItem>
         <NavTitleItem><NavTitle to='/notices' onClick={toggleMenu}>Find pet</NavTitle></NavTitleItem>
         <NavTitleItem><NavTitle to='/friends' onClick={toggleMenu}>Our friend</NavTitle></NavTitleItem>
     </NavTitleBox>)
 } 
-
-// export const Nav = () => {
-//     return(<ul style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', listStyle: 'none',}}>
-//         <li style={{marginRight: '80px'}}><a>News</a></li>
-//         <li style={{marginRight: '80px'}}><a>Find</a></li>
-//         <li><a>Our friend</a></li>
-//     </ul>)
-// }
