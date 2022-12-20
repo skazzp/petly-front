0e3dbee7a57447780d0ae91eddb86095913b99c1
seacat271 <seacat271083@gmail.com>
2022-12-20 23:37:59 +0200

    fixed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,12 @@
+import { useDispatch} from "react-redux";
 import { LogoNormal, LogoAccent, LogoLink } from "./Logo.styled"
+import { closeModal } from "redux/modal/modalSlice";
 
 export const Logo = () => {
+const dispatch = useDispatch()
+    const modalClosed = () => dispatch(closeModal())
     return (<div>
-        <LogoLink to='/'><LogoNormal>pe<LogoAccent>t</LogoAccent>ly</LogoNormal></LogoLink>
+        <LogoLink to='/' onClick={modalClosed}><LogoNormal>pe<LogoAccent>t</LogoAccent>ly</LogoNormal></LogoLink>
         </div>
     )
 }
