6cb9158f441aff811c6a5227dd65df347bd808cc
skazzp <skazzp@gmail.com>
2022-12-22 16:50:47 +0200

    cleanup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import { Logo } from "components/Logo"
-import { Navigation } from "components/Navigation"
-import { Container} from './Header.styled'
-
+import { Logo } from 'components/Logo';
+import { Navigation } from 'components/Navigation';
+import { Container } from './Header.styled';
 
 export const Header = () => {
-    return(<Container >
-        <Logo/>
-        <Navigation/>
-    </Container>)
-}
-
+  return (
+    <Container>
+      <Logo />
+      <Navigation />
+    </Container>
+  );
+};
