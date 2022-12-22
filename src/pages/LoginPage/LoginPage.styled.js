66008025a069eb001630e45d3b851ce3f67548ff
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-12-22 20:36:39 +0200

    imeges

--- src/pages/LoginPage/LoginPage.styled.js
+++ src/pages/LoginPage/LoginPage.styled.js
@@ -1,19 +1,18 @@
 import styled from 'styled-components';
 
 export const Div = styled.div`
-  padding-bottom: auto;
   width: 100vw;
-  height: 100%;
   display: flex;
   justify-content: center;
-  background: #fdf7f2;
   padding-top: 42px;
 
   @media screen and (min-width: 768px) {
     padding-top: 204px;
+    padding-bottom: 299px;
   }
 
   @media screen and (min-width: 1280px) {
     padding-top: 80px;
+    padding-bottom: 147px;
   }
 `;
