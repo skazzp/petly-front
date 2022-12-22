372e63d5491cb8aaed79781ffd6cbb268ec520b6
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-12-22 23:14:17 +0200

    Update FilterBtn.jsx

--- src/components/FilterBtn/FilterBtn.jsx
+++ src/components/FilterBtn/FilterBtn.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectToken } from 'redux/auth/authSelectors';
-import { getAllNotices, getByCategory } from 'redux/notice/noticeOperations';
+import { getByCategory } from 'redux/notice/noticeOperations';
 import { v4 as uuidv4 } from 'uuid';
 
 import { Button, FilterList, Item, Wrapper } from './FilterBtn.styled';
-import {
-  redirect,
-  useLocation,
-  useNavigate,
-  useSearchParams,
-} from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import AddNoticeButton from '../AddNoticeButton/AddNoticeButton';
-import { selectNotices } from 'redux/notice/noticeSelectors';
-import { useState } from 'react';
 
 const buttons = [
   {
@@ -46,51 +39,23 @@ function FilterBtn() {
   const token = useSelector(selectToken);
   const dispatch = useDispatch();
 
-  const [flag, setFlag] = useState(true);
-
   const [search] = useSearchParams();
 
   const page = search.get('page');
 
-  const navigate = useNavigate();
-
   const location = useLocation();
   const category = location.pathname.split('/')[2];
+
   // const handleClick = e => {
-  //   console.log(e.target.name, category);
-  //   console.log(location);
   //   if (e.target.name === category) {
-  //     return redirect('/friends/');
-
-  //   }
   //   return dispatch(getByCategory({ category: e.target.name, page }));
-  //   // if (flag) {
-  //   //   setFlag(false);
-  //   //   return dispatch(getByCategory({ category: e.target.name, page }));
-  //   // }
-  //   // setFlag(true);
-  //   // // dispatch(getAllNotices(page));
-  //   // return navigate('/notices');
+  //   }
   // };
-  console.log(flag);
-
   useEffect(() => {
     if (category) {
       dispatch(getByCategory({ category: category, page }));
     }
   }, [category, dispatch, location.pathname, page]);
-  const notices = useSelector(selectNotices);
-  // console.log(notices[0]?.category, category);
-
-  // const navigate = useNavigate();
-  // const l = e => {
-  //   if (notices[0]?.category === category) {
-  //     console.log(navigate);
-  //     return navigate('/notices');
-  //     // dispatch(getAllNotices(page));
-  //   }
-  //   dispatch(getByCategory({ category: e.target.name, page }));
-  // };
 
   return (
     <Wrapper>
