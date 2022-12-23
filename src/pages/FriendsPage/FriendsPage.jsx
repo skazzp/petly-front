7f108d89cf3bb7e0e63798b65164ebab0e8c6c4b
Denis <fox.ruden@gmail.com>
2022-12-23 03:15:41 +0200

    save

--- src/pages/FriendsPage/FriendsPage.jsx
+++ src/pages/FriendsPage/FriendsPage.jsx
@@ -1,6 +1,6 @@
 
 import Friends from 'components/Friends/Friends';
-import { ItemCard, ListCard, Title, Wrapper } from './FriendsPage.styled';
+import { ItemCard, ListCard, Title, Wrapper, WrapperList } from './FriendsPage.styled';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOurFriends } from 'redux/ourFriend/ourFriendOperations';
@@ -17,7 +17,7 @@ const FriendsPage = () => {
   return (
     <Wrapper>
       <Title>Our friend</Title>
-      {/* <WrapperList> */}
+      <WrapperList>
       <ListCard>
         {dataFriends?.map(value => (
           <ItemCard key={value?._id}>
@@ -25,7 +25,7 @@ const FriendsPage = () => {
           </ItemCard>
         ))}
       </ListCard>
-      {/* </WrapperList> */}
+      </WrapperList>
       
     </Wrapper>
   );
