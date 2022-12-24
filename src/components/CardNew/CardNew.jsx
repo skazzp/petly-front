04955ff0ece926cac9070a853ab7e57dcecb13ef
Denis <fox.ruden@gmail.com>
2022-12-25 00:29:40 +0200

    fix_NewPage

--- src/components/CardNew/CardNew.jsx
+++ src/components/CardNew/CardNew.jsx
@@ -1,15 +1,28 @@
 import {
+  Checkbox,
   Content,
   Data,
   Footer,
+  Label,
   Link,
+  Span,
   Strip,
   Title,
   Wrapper,
 } from './CardNew.styled';
+import { v4 as uuidv4 } from 'uuid';
 
 export const CardNew = ({ news }) => {
   const { title, url, description, date } = news;
+
+  const id = uuidv4();
+
+  const newDescriptionStart = description.slice(0, 200);
+  const newDescriptionEnd = description.slice(200);
+
+  const more = () => {
+    return <a href="#">more</a>;
+  };
 
   return (
     <>
@@ -17,7 +30,12 @@ export const CardNew = ({ news }) => {
         <div>
           <Strip />
           <Title>{title || 'no title'}</Title>
-          <Content>{description || 'no description'}</Content>
+          <Content>
+            <p><Checkbox type="checkbox" id={id} />
+            {newDescriptionStart || 'no description'}`
+            <Span>{newDescriptionEnd}</Span>
+            <Label for={id} /></p>
+          </Content>
         </div>
         <Footer>
           <Data>{date || ''}</Data>
