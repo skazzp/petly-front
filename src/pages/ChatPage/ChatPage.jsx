cf67779f8dcdc09dc3266fd084b4ec275b563a61
skazzp <skazzp@gmail.com>
2022-12-25 22:46:33 +0200

    Update ChatPage.jsx

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -1,45 +1,56 @@
-import { GlobalStyleRegistration ,Div} from "pages/RegistrationPage/RegistrationPage.styled";
-import { Validation } from "utility/validationStyle";
-import { ButtonSend,DivBox, Form, Input, Label, SelectContainer , Title } from "./ChatPage.styled";
+import {
+  GlobalStyleRegistration,
+  Div,
+} from 'pages/RegistrationPage/RegistrationPage.styled';
+// import { Validation } from "utility/validationStyle";
+import {
+  ButtonSend,
+  DivBox,
+  Form,
+  Input,
+  Label,
+  SelectContainer,
+  Title,
+} from './ChatPage.styled';
 import Select from 'react-select';
 
 const Chat = () => {
-  return (<Div>
-       <DivBox>
-      <Title>Chat </Title>
-        <Form onSubmit={"sad"}>
+  return (
+    <Div>
+      <DivBox>
+        <Title>Chat </Title>
+        <Form onSubmit={'sad'}>
           <Label>
-            <Input
-            ></Input>
+            <Input></Input>
             {/* {formik.errors.name && formik.touched.name ? (
               <Validation>{formik.errors.name}</Validation>
             ) : null} */}
           </Label>
-        <Label>
-          <SelectContainer>
+          <Label>
+            <SelectContainer>
               <Select
-                // placeholder="City"
-                // defaultValue={city}
-                // id="city"
-                //   name="city"
-                //   onBlur={formik.handleBlur}
-                // styles={selectStyles()}
-                // options={Selectoptions}
-                // onChange={e => setCity(e.value)}
-                // defaultInputValue={city}
+              // placeholder="City"
+              // defaultValue={city}
+              // id="city"
+              //   name="city"
+              //   onBlur={formik.handleBlur}
+              // styles={selectStyles()}
+              // options={Selectoptions}
+              // onChange={e => setCity(e.value)}
+              // defaultInputValue={city}
               ></Select>
-            </SelectContainer></Label>
-            
+            </SelectContainer>
+          </Label>
 
-            {/* {formik.errors.city && formik.touched.city ? (
+          {/* {formik.errors.city && formik.touched.city ? (
               <Validation>{formik.errors.city}</Validation>
             ) : null} */}
-          <ButtonSend> Send
-          </ButtonSend>
+          <ButtonSend> Send</ButtonSend>
         </Form>
-    </DivBox>
-      <GlobalStyleRegistration/>
-    </Div>);
-}
- 
+      </DivBox>
+      <GlobalStyleRegistration />
+    </Div>
+  );
+};
+
 export default Chat;
