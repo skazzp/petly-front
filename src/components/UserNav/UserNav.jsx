10e8a003bf96c8a7fd2c4774a54d34e954cbc43a
skazzp <skazzp@gmail.com>
2022-12-26 22:34:47 +0200

    Update UserNav.jsx

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,15 +1,15 @@
 import icon from '../../assets/images/icons.svg';
-import {Div, Link, UserSvg } from '../UserNav/UserNav.styled';
+import { Div, Link, UserSvg } from '../UserNav/UserNav.styled';
 
-export const UserNav = ({ closeMenu}) => {
+export const UserNav = ({ closeMenu }) => {
   return (
     <Div>
-      <Link to="/chat" onClick={closeMenu}>
+      {/* <Link to="/chat" onClick={closeMenu}>
         <UserSvg>
           <use href={icon + `#chat`}></use>
         </UserSvg>
         <span>Chat</span>
-      </Link>
+      </Link> */}
       <Link to="/user" onClick={closeMenu}>
         <UserSvg>
           <use href={icon + `#avatar-account`}></use>
