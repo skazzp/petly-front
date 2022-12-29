dc2dc0295f826f2eb02be523656334e5e28cc06b
seacat271 <seacat271083@gmail.com>
2022-12-29 12:36:54 +0200

    fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,17 @@
-import { useDispatch} from "react-redux";
-import { LogoNormal, LogoAccent, LogoLink } from "./Logo.styled"
-import { closeModal } from "redux/modal/modalSlice";
+import { useDispatch } from 'react-redux';
+import { closeModal } from 'redux/modal/modalSlice';
+import { LogoNormal, LogoAccent, LogoLink } from './Logo.styled';
 
 export const Logo = () => {
-const dispatch = useDispatch()
-    const modalClosed = () => dispatch(closeModal())
-    return (<div>
-        <LogoLink to='/' onClick={modalClosed}><LogoNormal>pe<LogoAccent>t</LogoAccent>ly</LogoNormal></LogoLink>
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+  const modalClosed = () => dispatch(closeModal());
+  return (
+    <div>
+      <LogoLink to="/" onClick={modalClosed}>
+        <LogoNormal>
+          pe<LogoAccent>t</LogoAccent>ly
+        </LogoNormal>
+      </LogoLink>
+    </div>
+  );
+};
--- src/components/ModalNav/ModalNav.jsx
+++ src/components/ModalNav/ModalNav.jsx
@@ -1,8 +1,8 @@
-import { ModalContainer } from './ModalNav.styled';
 import { createPortal } from 'react-dom';
 import { useEffect } from 'react';
-const modalRoot = document.querySelector('#modal-root');
+import { ModalContainer } from './ModalNav.styled';
 
+const modalRoot = document.querySelector('#modal-root');
 export const ModalNav = ({ children, header }) => {
   useEffect(() => {
     document.body.style.overflow = 'hidden';
