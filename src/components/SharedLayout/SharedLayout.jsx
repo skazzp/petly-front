81602da56f898cce81994442260d1186b75a5f6e
seacat271 <seacat271083@gmail.com>
2022-12-29 12:37:55 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import { LoaderSpinner } from 'components/LoaderSpiner/LoaderSpiner';
 import { Header } from 'components/Header';
 import { GlobalContainer } from './SharedLayout.styled';
-import { Suspense } from 'react';
-import { LoaderSpiner } from 'components/LoaderSpiner/LoaderSpiner';
 
 export const SharedLayout = () => {
   return (
     <>
       <GlobalContainer>
         <Header />
-        <Suspense fallback={<LoaderSpiner />}>
+        <Suspense fallback={<LoaderSpinner />}>
           <Outlet />
         </Suspense>
       </GlobalContainer>{' '}
